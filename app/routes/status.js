c58e1df69225986aab09b9450737a332109fd946
nfrunza <nfrunza@gmail.com>
2018-02-13 14:51:01 -0500

    BE-123, BE-124
    clenup trailing whitespaces from project
    
    Change-Id: I34023acc5e91e5ceee8b64955e43f6c169e4f5e5
    Signed-off-by: nfrunza <nfrunza@gmail.com>

---
+++ app/routes/status.js
@@ -0,0 +1,14 @@
+/***Status
+GET /api/status/get - > /api/status
+ */
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function getStatus(req, res) {
+    let idx = getRandomInt(1, 5);
+    let data = require('../../mock_server/mockData/apistatusget' + idx + '.json')
+    res.send(data);
+}
+
+module.exports = { getStatus };
