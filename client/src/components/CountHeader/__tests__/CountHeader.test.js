693867d7e569db898255e4c976821c825c6ebce8
nfrunza <nfrunza@gmail.com>
2018-02-12 20:10:43 -0500

    BE-123, BE-124
    added mock data, tests for api calls
    removed Trailing white spaces, jenkins failed the build
    
    Change-Id: I8fea8151f40c289eb32dbb898e9bd3df5db0e512
    Signed-off-by: nfrunza <nfrunza@gmail.com>

--- client/src/components/CountHeader/__tests__/CountHeader.test.js
+++ client/src/components/CountHeader/__tests__/CountHeader.test.js
@@ -4,24 +4,19 @@ import Adapter from 'enzyme-adapter-react-16';
 import configureStore from 'redux-mock-store';
 import CountHeader from "../CountHeader";
 
-const middlewares = []; // you can mock any middlewares here if necessary
+const middlewares = [];
 const mockStore = configureStore(middlewares);
-
 const initialState = {
     state: { isChaincodeView: true, isTransactionView: false },
 };
 
 describe('Testing CountHeader', () => {
-    let wrapper
+    let wrapper;
     it('renders as expected', () => {
         wrapper = shallow(
             <CountHeader />,
             { context: { store: mockStore(initialState) } },
         );
         expect(wrapper.dive()).toMatchSnapshot();
-
-        it('wrapper length > 0 ', () => {
-            expect(wrapper.length).toEqual(1);
-        });
     });
 });
