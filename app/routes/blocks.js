cc660eb23a54f495e8f2c26cc797f2e066ce7d44
nfrunza <nfrunza@gmail.com>
2018-02-12 20:10:43 -0500

    BE-123, BE-124
    added mock data, tests for api calls
    removed Trailing white spaces, jenkins failed th build
    Change-Id: I8fea8151f40c289eb32dbb898e9bd3df5db0e512
    Signed-off-by: nfrunza <nfrunza@gmail.com>

---
+++ app/routes/blocks.js
@@ -0,0 +1,19 @@
+/****
+ * Block information
+Block by number
+
+GET /api/block/getinfo -> /api/block/id=2
+ */
+function getBlockById(req, res) {
+    let id = req.params.id
+    console.log('getChannelsByPeer:', id);
+    let data = require('../../mock_server/mockData/apiblockgetinfo.json')
+    if (id) {
+        res.send(data);
+    } else {
+        res.send({});
+    }
+};
+
+module.exports = { getBlockById };
+
