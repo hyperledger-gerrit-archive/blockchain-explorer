ff02afe8ef3dc62f01c6b56202c738ea9c712ff8
maedwards <dev.edwardsm@gmail.com>
2018-05-09 11:34:56 -0400

    BE-258
    Activity stream shows new blocks
    
    Commented out material-ui from Header.js
    
    Change-Id: I3ec7572f6a5b0d92906a6599e37d2bc2bd330932
    Signed-off-by: maedwards <dev.edwardsm@gmail.com>

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,39 +4,39 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-import Avatar from 'material-ui/Avatar';
+// import { withStyles } from 'material-ui/styles';
+// import AppBar from 'material-ui/AppBar';
+// import Toolbar from 'material-ui/Toolbar';
+// import Typography from 'material-ui/Typography';
+// import Avatar from 'material-ui/Avatar';
 
-const styles = theme => ({
-  root: {
-    width: '100%',
-    backgroundColor: theme.palette.background.paper,
-  },
+// const styles = theme => ({
+//   root: {
+//     width: '100%',
+//     backgroundColor: theme.palette.background.paper,
+//   },
 
-});
+// });
 
 
 function Header(props) {
-  const { classes } = props;
+  // const { classes } = props;
   return (
-    <div className={classes.root}>
-      <AppBar position="static" >
+    <div >
+      {/* <AppBar position="static" >
         <Toolbar>
           <Avatar src={process.env.PUBLIC_URL + '/favicon.ico'} alt='logoimage' className={classes.avatar} />
           <Typography type="title" color="inherit" className={classes.flex}>
           HYPERLEDGER EXPLORER
           </Typography>
         </Toolbar>
-      </AppBar>
+      </AppBar> */}
     </div>
   );
 }
 
-Header.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+// Header.propTypes = {
+//   classes: PropTypes.object.isRequired,
+// };
 
-export default withStyles(styles)(Header);
+export default /*withStyles(styles)*/(Header);
