464a36c3efd316d056baf8069606329476974c29
kavya <tkavya099@gmail.com>
2018-06-06 11:44:32 -0400

    BE-205 ADD Swagger for Hyperledger
    Integrated the swagger
    
    Change-Id: Ibe9b533d563cefbca0b097945227ac753d1d55b2
    Signed-off-by: kavya <tkavya099@gmail.com>

--- main.js
+++ main.js
@@ -29,6 +29,10 @@ var config = require("./appconfig.json");
 var PlatformBuilder = require("./app/platform/PlatformBuilder.js");
 
 var statusMetrics = require("./app/service/metricservice.js");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.static(path.join(__dirname, "client/build")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
