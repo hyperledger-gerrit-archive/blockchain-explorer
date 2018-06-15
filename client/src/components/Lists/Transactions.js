b77bd047ae494d343c548c8785fc48cd084e18aa
pranay <pranayl.developer@gmail.com>
2018-06-15 03:41:56 -0400

    BE-301
    Improved Transction Details modal's look and feel
    
    Change-Id: I6dde42c59f9cc9a2ccaefdb646bd7f6f0f96d091
    Signed-off-by: pranay <pranayl.developer@gmail.com>

--- client/src/components/Lists/Transactions.js
+++ client/src/components/Lists/Transactions.js
@@ -2,13 +2,13 @@
  *    SPDX-License-Identifier: Apache-2.0
  */
 
-import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
-import Dialog, { DialogTitle } from 'material-ui/Dialog';
-import TransactionView from '../View/TransactionView';
-import ReactTable from 'react-table';
-import 'react-table/react-table.css';
-import matchSorter from 'match-sorter';
+import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
+import Dialog, { DialogTitle } from "material-ui/Dialog";
+import TransactionView from "../View/TransactionView";
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+import matchSorter from "match-sorter";
 
 class Transactions extends Component {
   constructor(props) {
@@ -39,38 +39,68 @@ class Transactions extends Component {
         Header: "Creator",
         accessor: "creator_msp_id",
         filterMethod: (filter, rows) =>
-          matchSorter(rows, filter.value, { keys: ["creator_msp_id"] }, { threshold: matchSorter.rankings.SIMPLEMATCH }),
+          matchSorter(
+            rows,
+            filter.value,
+            { keys: ["creator_msp_id"] },
+            { threshold: matchSorter.rankings.SIMPLEMATCH }
+          ),
         filterAll: true
       },
       {
         Header: "Tx Id",
         accessor: "txhash",
         Cell: row => (
-          <a onClick={() => this.handleDialogOpen(row.value)} href="#/transactions" >{row.value}</a>
+          <a
+            onClick={() => this.handleDialogOpen(row.value)}
+            href="#/transactions"
+          >
+            {row.value}
+          </a>
         ),
         filterMethod: (filter, rows) =>
-          matchSorter(rows, filter.value, { keys: ["txhash"] }, { threshold: matchSorter.rankings.SIMPLEMATCH }),
+          matchSorter(
+            rows,
+            filter.value,
+            { keys: ["txhash"] },
+            { threshold: matchSorter.rankings.SIMPLEMATCH }
+          ),
         filterAll: true
       },
       {
         Header: "Type",
         accessor: "type",
         filterMethod: (filter, rows) =>
-          matchSorter(rows, filter.value, { keys: ["type"] }, { threshold: matchSorter.rankings.SIMPLEMATCH }),
+          matchSorter(
+            rows,
+            filter.value,
+            { keys: ["type"] },
+            { threshold: matchSorter.rankings.SIMPLEMATCH }
+          ),
         filterAll: true
       },
       {
         Header: "Chaincode",
         accessor: "chaincodename",
         filterMethod: (filter, rows) =>
-          matchSorter(rows, filter.value, { keys: ["chaincodename"] }, { threshold: matchSorter.rankings.SIMPLEMATCH }),
+          matchSorter(
+            rows,
+            filter.value,
+            { keys: ["chaincodename"] },
+            { threshold: matchSorter.rankings.SIMPLEMATCH }
+          ),
         filterAll: true
       },
       {
         Header: "Timestamp",
         accessor: "createdt",
         filterMethod: (filter, rows) =>
-          matchSorter(rows, filter.value, { keys: ["createdt"] }, { threshold: matchSorter.rankings.SIMPLEMATCH }),
+          matchSorter(
+            rows,
+            filter.value,
+            { keys: ["createdt"] },
+            { threshold: matchSorter.rankings.SIMPLEMATCH }
+          ),
         filterAll: true
       }
     ];
@@ -83,7 +113,8 @@ class Transactions extends Component {
           defaultPageSize={10}
           className="-striped -highlight"
           filterable
-          minRows={0}/>
+          minRows={0}
+        />
 
         <Dialog
           open={this.state.dialogOpen}
@@ -91,11 +122,14 @@ class Transactions extends Component {
           fullWidth={true}
           maxWidth={"md"}
         >
-          <TransactionView transaction={this.props.transaction} />
+          <TransactionView
+            transaction={this.props.transaction}
+            onClose={this.handleDialogClose}
+          />
         </Dialog>
       </div>
     );
   }
-};
+}
 
 export default Transactions;
