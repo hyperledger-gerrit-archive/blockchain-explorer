bc76447b4d236975fc10ed9fd7d29ab6de1079a7
Srinivasa Giddaluru <srinivas640@gmail.com>
2018-07-09 17:00:47 +0530

    BE 356 Compress HTTP Request and Response
    Change-Id: Ia1166fbc178cec94373f55cb9a3a0c886468ec4a
    Signed-off-by: Srinivasa Giddaluru <srinivas640@gmail.com>

--- app/explorer/Explorer.js
+++ app/explorer/Explorer.js
@@ -10,6 +10,8 @@ var PersistanceFactory = require("../persistance/PersistanceFactory.js");
 var timer = require("./backend/timer.js");
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../../swagger.json');
+var compression = require('compression');
+
 
 class Explorer {
     constructor() {
@@ -17,6 +19,7 @@ class Explorer {
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: true }));
         this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+        this.app.use(compression());
         this.persistance = {};
         this.platforms = explorerconfig["platforms"];
 
