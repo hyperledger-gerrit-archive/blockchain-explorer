1f5aa086323d7b81ed51a3eba98437386ebd0d3d
ArigelaSatyanarayana <arigelas@gmail.com>
2018-08-09 18:08:06 +0530

    Merging 3.5 and fab 1.2 integration
    Update missing file and merge fab 1.2.0 with release 3.5
    
    Change-Id: I1bb8203ef81e843b7bf859a619fd7f781aaf3515
    Signed-off-by: ArigelaSatyanarayana <arigelas@gmail.com>

--- app/platform/fabric/Proxy.js
+++ app/platform/fabric/Proxy.js
@@ -208,9 +208,9 @@ class Proxy {
       };
       this.broadcaster.broadcast(notify);
     } else if (fabric_const.NOTITY_TYPE_EXISTCHANNEL === msg.notify_type) {
-      throw new ExplorerError('Channel name [' +
+      throw new ExplorerError('Explorer is closing due to channel name [' +
         msg.channel_name +
-        '] is already exist in DB , Kindly re-run the DB scripts to proceed');
+        '] is already exist in DB');
     } else if (msg.error) {
       throw new ExplorerError('Client Processor Error >> ' + msg.error);
     } else {
