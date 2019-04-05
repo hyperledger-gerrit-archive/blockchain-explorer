e9d0fa9ac8f541d0da39ee016b11925111a921ec
Uma Parameshwari <umaparameswari11@gmail.com>
2019-04-05 17:32:22 +0530

    BE-597 Increase Code Coverage for UI Test
    * Added Spec files for enroll, register, login
    
    Change-Id: Ic3bff11b263d3de8ec8cfc7b67cf82510761027c
    Signed-off-by: Uma Parameshwari <umaparameswari11@gmail.com>

---
+++ client/src/components/Footer/FooterView.spec.js
@@ -0,0 +1,16 @@
+import FooterView from './FooterView';
+
+const setup = () => {
+  const wrapper = shallow(<FooterView />);
+
+  return {
+    wrapper
+  };
+};
+
+describe('FooterView', () => {
+  test('FooterView component should render', () => {
+    const { wrapper } = setup();
+    expect(wrapper.exists()).toBe(true);
+  });
+});
