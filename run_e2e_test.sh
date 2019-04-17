2a6e0cd282ce3f05e8f07ef789d839afbadf1648
Atsushi Neki <atsushin@fast.au.fujitsu.com>
2019-04-17 03:36:52 +0000

    BE-612 Create a script for running e2e test
    Change-Id: Ifa71e6ea8450d371d15ec6f8cc4f8a4e894c8c44
    Signed-off-by: Atsushi Neki <atsushin@fast.au.fujitsu.com>

---
+++ run_e2e_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Prerequisition:
+#   Install Hyperledger Fabric container images and tools (by running fabric-samples/scripts/bootstrap.sh)
+#   You also need to add these tool location to PATH
+
+npm install
+npm run e2e-test
